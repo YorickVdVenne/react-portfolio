07aa9bfcb9df76612a1650c17835322e4f203f7f
YorickVdVenne <yorick.vd.venne@hotmail.nl>
2020-06-15 20:03:39 +0200

    Added a contact form to contact page

--- src/content/ContactForm.js
+++ src/content/ContactForm.js
@@ -3,7 +3,21 @@ import React from 'react'
 export const ContactForm = () => {
     return (
         <div>
-            
+            <form>
+            <div class="form-group">
+                <label for="exampleFormControlInput1">Naam</label>
+                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Jouw naam..."/>
+            </div>
+            <div class="form-group">
+                <label for="exampleFormControlInput1">Email adres</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Jouw email adres..."/>
+            </div>
+            <div class="form-group">
+                <label for="exampleFormControlTextarea1">Bericht</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">Verstuur</button>
+            </form>
         </div>
     )
 }
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { ContactForm } from '../content/ContactForm'
 
 export const Contact = () => {
     return (
-        <div>
-            Contact
+        <div class="container">
+            <h1 class="m-5">Contact</h1>
+           <ContactForm />
         </div>
     )
 }
