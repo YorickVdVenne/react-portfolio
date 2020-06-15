49021325b20ddbc7f6cc75c136b5002a3fcf022b
YorickVdVenne <yorick.vd.venne@hotmail.nl>
2020-06-15 16:12:09 +0200

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,13 +5,25 @@ export const Header = () => {
     return (
         <div>
             <header>
-                <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-                    <div className="container">
-                        <Link to="/" className="navbar-brand text-white">
-                            <h1 className="display-4">React Portfolio</h1>
-                        </Link>
-                    </div>
-                </nav>
+            <nav class="navbar sticky-top navbar-expand-md bg-dark navbar-dark">
+                <Link to={'/'} class="navbar-brand" > React Portfolio </Link>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                    <Link to={'/'} class="nav-link" > Home </Link>
+                    </li>
+                    <li class="nav-item">
+                    <Link to={'/about'} class="nav-link" >About Me</Link>
+                    </li>    
+                    <li class="nav-item">
+                    <Link to={'/contact'} class="nav-link" >Contact</Link>
+                    </li>  
+                </ul>
+                </div>  
+            </nav>
             </header>
         </div>
     )
