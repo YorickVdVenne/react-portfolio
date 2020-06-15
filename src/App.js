c0c38368ed95235e4cc9780c46e422f93bc8f9c3
YorickVdVenne <yorick.vd.venne@hotmail.nl>
2020-06-15 16:14:12 +0200

    Updated routing

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Header } from './components/Header'
 
 import { Home } from './pages/Home'
+import { About } from './pages/About'
+import { Contact } from './pages/Contact'
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
       <div className="App">
         <Switch>
           <Route path="/" exact component={Home}/>
+          <Route path="/about" exact component={About}/>
+          <Route path="/contact" exact component={Contact}/>
         </Switch>
       </div>
 
